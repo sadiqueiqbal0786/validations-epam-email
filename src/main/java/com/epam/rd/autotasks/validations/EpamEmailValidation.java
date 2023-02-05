6aeb7e138fb08effb7bec37518815d026e18933f
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-02-05 19:23:36 +0530

    done

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -4,13 +4,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EpamEmailValidation {
+    public static boolean validateEpamEmail(String input) {
+        if (input == null) {
+            return false;
+        }
 
-    public static boolean validateEpamEmail(String email) {
-        // Put your code here
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+_[a-zA-Z]+[0-9]*@epam.com$");
+        Matcher matcher = pattern.matcher(input);
 
+        return matcher.matches();
     }
 }
-
+/*
+* This method uses regular expressions to check if the
+* input string matches the pattern of a regular EPAM email.
+* The email must start with a first name and last name (in English),
+* separated by an underscore (_), and end with @epam.com.
+*  The first name and last name may be followed by an optional number.
+* If the input string matches this pattern, the method returns true;
+* otherwise, it returns false.
+* */
 
 
 
